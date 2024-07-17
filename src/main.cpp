25e6a3a9f95cbe88d6a4a74f4989e30412901f0c
AbhijeetJ-512 <01fe21bei045@kletech.ac.in>
2024-07-17 14:45:30 +0530

    Implement user registration and basic login functionality with secure password input.

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,66 @@
 #include <iostream>
-#include <string>
+#include <termios.h>
+#include <unistd.h>
+
 using namespace std;
 
+string getPassword() {
+  string password;
+  struct termios oldt, newt;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+
+  newt.c_lflag &= ~ECHO;
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+  cout << "password: ";
+  cin >> password;
+  cout << endl;
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+  return password;
+}
+
+bool RegisterUser() {
+  string username, password1, password2;
+  cout << "Register\n";
+  cout << "Enter username: ";
+  cin >> username;
+  do {
+    cout << "Enter ";
+    password1 = getPassword();
+    cout << "Verify ";
+    password2 = getPassword();
+    if (password1 != password2) {
+      cout << "Passwords do not match. Please try again.\n";
+    }
+  } while (password1 != password2);
+  return true;
+}
+
+bool LoginUser() {
+  string username, password;
+  cout << "Register\n";
+  do {
+    cout << "Enter username: ";
+    cin >> username;
+    cout << "Enter ";
+    password = getPassword();
+  } while (0); // Condition will be Authentication of username and password in
+               // database;
+  return true;
+}
 int main() {
   int choice;
-  do {
-    cout << "1. Add Password\n2. Retrieve Password\n3. Exit\n";
-    cin >> choice;
-    if (choice == 1) {
-      string site, username, password;
-      cout << "Enter site: ";
-      cin >> site;
-      cout << "Enter username: ";
-      cin >> username;
-      cout << "Enter password: ";
-      cin >> password;
-    } else if (choice == 2) {
-      string site, username;
-      cout << "Enter site: ";
-      cin >> site;
-      cout << "Enter username: ";
-      cin >> username;
-    }
-  } while (choice != 3);
+  cout << "1. Register\n2. Login\n" << endl;
+  if (choice == 1) {
+    RegisterUser();
+  } else if (choice == 2) {
+    LoginUser();
+  } else {
+    cout << "Invalid choice. Exiting program." << endl;
+  }
   return 0;
 }
