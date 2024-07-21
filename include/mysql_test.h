9218c680de564abb97bd16a6d52dae722e41258a
Sheshank Patil <01fe21bei037@kletech.ac.in>
2024-07-21 15:40:37 +0530

    x X
     Please enter the commit message for your changes. Lines starting

--- include/mysql_test.h
+++ include/mysql_test.h
@@ -10,6 +10,7 @@ void finish_with_error(MYSQL *con);
 int insert_user(MYSQL *con);
 void insert_password(MYSQL *con, int user_id);
 void retrieve_user_password(MYSQL *con, int user_id);
+int login_user(MYSQL *con);
  
 
 #endif
