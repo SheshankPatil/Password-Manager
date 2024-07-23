2fbce0a3e33b1aab7e5bd833448a023232ec229c
JadenEkbote <01fe21bei043@kletech.ac.in>
2024-07-23 12:46:49 +0530

    added encyrption to given hashed password using openssl's aes-256-cbc encrytpion. Used the same for decryption

---
+++ src/encyrpt.cpp
@@ -0,0 +1,39 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+string encryption(const string &password, const string &text, const string &master_pass)
+{
+    string command =
+        "echo -n \"" + text +
+        "\" | openssl enc -aes-256-cbc -e -base64 -pbkdf2 -pass pass:" + password;
+    string encText = executeCommand(command);
+    if (!encText.empty())
+    {
+        cout << encText << endl;
+    }
+    else
+    {
+        cerr << "Encryption failed!" << endl;
+    }
+    return encText;
+}
+
+int decryption(const string &password, const string &enc, const string &master_pass)
+{
+    string command =
+        "echo \"" + enc +
+        "\" | openssl enc -aes-256-cbc -d -base64 -pbkdf2 -pass pass:" + password;
+    string decText = executeCommand(command);
+    if (!decText.empty())
+    {
+        cout << "Decrypted text: " << decText << endl;
+        return 0; // Successful decryption
+    }
+    else
+    {
+        cerr << "Decryption failed!" << endl;
+        return -1; // Decryption failed
+    }
+}
